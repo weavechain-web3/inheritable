81e1fe67a455bc7af9eb5f8622af426243084eb6
ZILECAO <zilecao@sas.upenn.edu>
2023-03-04 00:07:02 -0700

    updated oracle addresses

--- src/Reader.js
+++ src/Reader.js
@@ -416,14 +416,14 @@ class Reader extends Component {
                             className="px-5 py-2.5 text-sm font-medium text-white bg-yellow-600 hover:bg-yellow-700 rounded-md shadow"
                             type="submit" onClick={() => this.readClaim()}>
 
-                            Read Claim
+                            Check Eligibility
                         </button>
                         &nbsp;
                         <button
                             className="px-5 py-2.5 text-sm font-medium text-white bg-yellow-600 hover:bg-yellow-700 rounded-md shadow"
                             type="submit" onClick={() => this.check()}>
 
-                            Check Claim
+                            Verify Claim
                         </button>
                     </p>
                 </div>
